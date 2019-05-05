eabac320c303306f652cdac48a6c9c7c1235395d
k-okada-bold <k_okada@bold.ne.jp>
2019-05-05 14:50:01 +0900

    ログイン情報をAWSに変更

--- LoginSample2/src/login/LoginServlet.java
+++ LoginSample2/src/login/LoginServlet.java
@@ -6,8 +6,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -15,8 +13,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import bean.User;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
@@ -44,27 +40,27 @@ public class LoginServlet extends HttpServlet {
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/javasystem","root","root");
+                    "jdbc:mysql://javasystem-demo.cwcncfovruyw.us-east-2.rds.amazonaws.com:3306/javasystem","root","javasystem");
             ps = con.prepareStatement(
                     "select user_name from user where user_id = ? and password = ?");
             ps.setString(1, userId);
             ps.setString(2, password);
             rs = ps.executeQuery();
 
-            List<User> list = new ArrayList<User>();
-            ps2 = con.prepareStatement(
-                    "select * from user;");
-
-            rs2 = ps2.executeQuery();
-            while (rs2.next()) {
-				String _user_id = rs2.getString("user_id");
-				String _user_name = rs2.getString("user_name");
-				String _password = rs2.getString("password");
-				list.add(new User(_user_id,_user_name,_password));
-			}
-
-
-            request.setAttribute("users", list);
+//            List<User> list = new ArrayList<User>();
+//            ps2 = con.prepareStatement(
+//                    "select * from user;");
+//
+//            rs2 = ps2.executeQuery();
+//            while (rs2.next()) {
+//				String _user_id = rs2.getString("user_id");
+//				String _user_name = rs2.getString("user_name");
+//				String _password = rs2.getString("password");
+//				list.add(new User(_user_id,_user_name,_password));
+//			}
+//
+//
+//            request.setAttribute("users", list);
             String userName = null;
             while(rs.next()) {
                 userName = rs.getString("user_name");
